798bb2c0d3a0773187a60a882e66cbafb9d72b12
AkibReza <akib.reza23@gmail.com>
2022-07-12 07:22:56 +0600

    destination image size fixed

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      spacing: {
+        164: "41rem",
+      },
+    },
+  },
+  plugins: [],
+};
