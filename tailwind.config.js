3dea6d70b16b58f3e29712199c12301e72e1e2c4
AkibReza <akib.reza23@gmail.com>
2022-07-23 23:53:36 +0600

    Added banner

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,14 @@ module.exports = {
       spacing: {
         164: "41rem",
       },
+      colors: {
+        primary: "#4a4a4a",
+        secondary: "#b2b2b2",
+        btnPrimary: "#fbad27",
+        btnHover: "#ebbe35",
+        navRule: "#8795a9",
+        footer: "#616161",
+      },
     },
   },
   plugins: [],
